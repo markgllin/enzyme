6a12cfc9b4c9845330556ad0c40378691eb6c463
markgllin <markgllin@hotmail.com>
2022-08-15 21:24:07 -0700

    pass first test case

---
+++ enzyme.rb
@@ -0,0 +1,52 @@
+class CraqValidator
+
+    attr_reader :errors
+
+    def initialize(questions, answers)
+        @questions = []
+        @errors = {}
+
+        questions.each_with_index do |q, i| 
+            @questions << Question.new(q, i, answers[:"q#{i}"])
+
+            if !@questions[i].is_valid_answer?
+                @valid = false
+                @errors[:"q#{i}"] = @questions[i].error
+            end
+        end
+    end
+
+    def valid?
+        @valid
+    end
+
+end
+
+class Question
+
+    attr_reader :error
+
+    def initialize(question, number, answer)
+        @question_number = number
+        @question = question[:text]
+        @opts = question[:options]
+        @answer = answer
+        @error = nil
+    end
+
+    def is_valid_answer?
+        if @answer.nil?
+            @error = 'was not answered'
+            return false
+        elsif !@answer.between?(1, @opts.length)
+            @error = 'has an answer that is not on the list of valid answers'
+            return false
+        end
+        
+        true
+    end
+
+    def error
+        @error
+    end
+end
